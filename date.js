2f31e671891106baf20383757b3b1af20c8f5efc
melkor37 <36717247+melkor37@users.noreply.github.com>
2018-02-22 13:26:54 -0500

    .02.22.13.26
    Successfully creates GolarianDate, adds attributes but will duplicate if run more than once

--- date.js
+++ date.js
@@ -1,5 +1,11 @@
 //Defines me as the almighty
-var almighty = "melkor"
+var almighty = "melkor";
+var gdday = ['Sunday','Moonday','Toilday','Wealday','Oathsday','Fireday','Starday'];
+var gdmonth = ['Abadius','Calistril','Pharast','Gozran','Desnus','Sarenith','Erastus','Arodus','Rova','Lamashan','Neth','Kuthona'];
+var gdCurdate findObjs({
+        type: 'character',
+        name: 'Golarian.Dates'
+    })[0];
 
 /*Initially checks to see if the character Golarian.Dates exists
   if not, creates.  SHOULD create the attributes I need on the character,
@@ -18,40 +24,125 @@ var almighty = "melkor"
   
   
  */ 
-createDate = function(dd,mm,yy){
+createDate= function(dd, mm, yy) {
+    // SNARF: Create variable
+    var golarianDate;
+ 
     //function to find Golarian.Dates
-	golarianDate = findObjs({
+    golarianDate = findObjs({
+        type: 'character',
+        name: 'Golarian.Dates'
+    })[0];
+ 
+    //if fail, create..
+    if(!golarianDate) {
+        // SNARF: Create and set the 'golarianDate' variable right away if it can't be found
+        golarianDate = createObj('character', {
+            name: 'Golarian.Dates'
+        });
+    }
+    //This is where I want to create the attributes
+    // SNARF: I expect you to have full access to 'golarianDate' variable here...
+ 
+    //should automatically be true
+    if(golarianDate) {
+        sendChat("God Almighty", "Success!")
+        //Ideally, attribute creation should go here..
+        createObj("attribute", {
+            name: "gmonth",
+            current: 0,
+            characterid: golarianDate.id
+            });
+        createObj("attribute", {
+            name: "gmonthtext",
+            current: 'Abadius',
+            characterid: golarianDate.id
+            });
+        createObj("attribute", {
+            name: "gday",
+            current: 0,
+            characterid: golarianDate.id
+            });
+        createObj("attribute", {
+            name: "gdaytext",
+            current: "Moonday",
+            characterid: golarianDate.id
+            });
+        createObj("attribute", {
+            name: "gdate",
+            current: 1,
+            characterid: golarianDate.id
+            });
+        createObj("attribute", {
+            name: "gyear",
+            current: 4718,
+            characterid: golarianDate.id
+            });
+        }
+        
+        
+        
+        
+};
+
+//Check to see if attributes exist
+checkAtt = function(mm,dd,yy){
+    var gdatt;
+    var golarianDate;
+ 
+    //function to find Golarian.Dates
+    golarianDate = findObjs({
         type: 'character',
         name: 'Golarian.Dates'
     })[0];
     
-    //if fail, create..
-	if (!golarianDate){
-        GolarianDate = createObj('character', {
-            name: 'Golarian.Dates'
-		    });
-        };
-    //This is where I want to create the attributes
-	
-	//should automatically be true
-	if(golarianDate){
-        sendChat("God Almighty","Success!")
-        //Ideally, attribute creation should go here..
+    gdatt = findObjs({
+        type: 'attribute',
+        name: 'gmonth',
+        characterid: golarianDate.id
+    })[0];
+    /*
+    if(!gdatt) {
+        sendChat(almighty, "gmonth doesn't exist");
+    }
+    if(gdatt) {
+        sendChat(almighty, "gdatt" + gdatt + "gmonth does exist.");
+        sendChat(almighty,"gdatt: " + gdatt);
+        sendChat(almighty,"golarianDate: " + golarianDate);
     };
+    */
 };
 
 //Lets me know in chat log that APIs are finished loading
 on("ready", function(dd,mm,yy) {
-    sendChat(almighty,"<b>Loaded.<b>");
+    sendChat(almighty,"<hr><b>Loaded.<b>");
     createDate();
     sendChat(almighty,"Ran check")
 });    
 
-//Listens for '!gd' in chat, runs createDate
+//Listens for '!gd --create' in chat, runs createDate
 on("chat:message", function(msg) {
-    if(msg.type == "api" && msg.content.indexOf("!gd") !== -1){
+    if(msg.type == "api" && msg.content.indexOf("!gd --create") !== -1){
         createDate();
         sendChat(almighty,"Ran check");
+        }
+    });
+
+//Listens for '!gd --check' to see if script is running
+	on("chat:message", function(msg) {
+    if(msg.type == "api" && msg.content.indexOf("!gd --check") !== -1){
+        createDate();
+        sendChat(almighty,"Script Running");
+        
+        }
+    });
+
+//Checks Golarian.Date to see if first attribute exists
+//For some reason, it keeps coming up positive
+	on("chat:message", function(msg) {
+    if(msg.type == "api" && msg.content.indexOf("!gd --gdatt") !== -1){
+        checkAtt();
+        sendChat(almighty,"checking if att exists");
         }
     });
 
